82da744c3e723eefdbcbb257d2b0057c8324c54b
Bohdan Solianyk <bohdan_solianyk@epam.com>
2024-06-23 15:25:02 +0300

    CFY-4 add TG BOT POC

---
+++ consultify_tg_bot/main.py
@@ -0,0 +1,62 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+import aiohttp
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("TG_BOT_TOKEN")
+# Your Telegram bot token
+# Your API Gateway endpoint
+API_GATEWAY_URL = getenv("APIGATEWAY_URL")
+
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        async with session.post(f"{API_GATEWAY_URL}/Prod/hello/", json={'query': message.text}) as response:
+            print(response)
+            if response.status == 200:
+                api_response = await response.json()
+                api_response_message = api_response.get('response', 'No response from API')
+            else:
+                api_response_message = f"Failed to reach API: {response.status}"
+
+    await message.reply(f"Message received: {message.text}\nAPI response: {api_response_message}")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
